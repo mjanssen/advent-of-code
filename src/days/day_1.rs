014ae08a5d176ac75f0b51e3a58f1bfeef923a7c
mjanssen <dev.marnix@gmail.com>
2022-12-01 13:29:03 +0100

    Day 1 completed

--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -1,6 +1,27 @@
 use crate::lib::load_file::load_data_file;
 
 pub fn execute() -> Result<(), Box<dyn std::error::Error>> {
-    let _data = load_data_file("day_1.txt")?;
+    let data = load_data_file("day_1.txt")?;
+
+    let mut lines: Vec<u32> = data
+        .split("\n\n")
+        .map(|x: &str| {
+            x.split("\n")
+                .map(|x: &str| match x.parse::<u32>() {
+                    Ok(v) => v,
+                    _ => 0,
+                })
+                .collect::<Vec<u32>>()
+        })
+        .map(|nums: Vec<u32>| nums.iter().sum())
+        .collect();
+
+    lines.sort();
+    println!("Day 1 - part 1 - {:?}", &lines);
+
+    let sum: u32 = lines.iter().rev().take(3).sum();
+
+    println!("Day 1 - part 2 - {:?}", &sum);
+
     Ok(())
 }
