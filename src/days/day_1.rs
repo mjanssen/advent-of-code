29ee3463c411a5897af4bf55fdf5e4ce88866e77
mjanssen <dev.marnix@gmail.com>
2022-12-01 10:58:35 +0100

    Updated data import for day1

--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -1,6 +1,6 @@
 use crate::lib::load_file::load_data_file;
 
 pub fn execute() -> Result<(), Box<dyn std::error::Error>> {
-    let _data = load_data_file("day1.txt")?;
+    let _data = load_data_file("day_1.txt")?;
     Ok(())
 }
