74d053544844bd1f69baf0926e33d780568956c4
mjanssen <dev.marnix@gmail.com>
2022-12-11 15:30:14 +0100

    Day 10

--- src/days/day_10.rs
+++ src/days/day_10.rs
@@ -9,8 +9,12 @@ pub fn execute() -> ExecuteResponse {
 
     let mut cycles = 0;
     let mut x_register = 1;
+
+    let mut crt_cycles = 0;
+
     let mut signal_strength: HashMap<i32, i32> = HashMap::new();
 
+    println!("Part 2 word");
     data.lines().for_each(|line| {
         let mut x_add = 0;
         let mut cycle_amount = 1;
@@ -26,12 +30,26 @@ pub fn execute() -> ExecuteResponse {
         (0..cycle_amount).for_each(|_| {
             cycles += 1;
 
+            // Part 1
             if cycles == 20 {
                 signal_strength.insert(cycles, cycles * x_register);
-                println!("multiply {} * {}", cycles, x_register);
             } else if (cycles - 20) % 40 == 0 {
                 signal_strength.insert(cycles, cycles * x_register);
-                println!("multiply {} * {}", cycles, x_register);
+            }
+
+            // // Part 2 / cycles -1 since the cycles already start counting before, we want to start at 0
+            crt_cycles = (cycles - 1) % 40;
+
+            let range = x_register - 1..x_register + 2;
+
+            if range.contains(&crt_cycles) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+
+            if crt_cycles == 39 {
+                println!(" <- Cycle {}", cycles);
             }
         });
 
@@ -39,7 +57,8 @@ pub fn execute() -> ExecuteResponse {
     });
 
     let answer: i32 = signal_strength.into_iter().map(|(_cycle, num)| num).sum();
-    println!("part 1 - {}", answer);
+
+    println!("\npart 1 - {}", answer);
 
     Ok(())
 }
