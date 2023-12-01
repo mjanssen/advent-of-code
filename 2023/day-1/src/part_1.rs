6e65d6cebc7ee440b8882d97309fdeb47a585f02
mjanssen <dev.marnix@gmail.com>
2023-12-01 17:52:08 +0100

    chore: make parts one-liners

--- 2023/day-1/src/part_1.rs
+++ 2023/day-1/src/part_1.rs
@@ -1,14 +1,15 @@
 pub fn process(_input: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let result = _input.lines().fold(0, |mut ans, _line| {
-        let nums = _line
-            .chars()
-            .filter_map(|x| x.to_digit(10))
-            .collect::<Vec<u32>>();
+    Ok(_input
+        .lines()
+        .fold(0, |mut ans, _line| {
+            let nums = _line
+                .chars()
+                .filter_map(|x| x.to_digit(10))
+                .collect::<Vec<u32>>();
 
-        ans += (10 * nums.first().unwrap_or(&0)) + nums.last().unwrap_or(&0);
+            ans += (10 * nums.first().unwrap_or(&0)) + nums.last().unwrap_or(&0);
 
-        ans
-    });
-
-    Ok(result.to_string())
+            ans
+        })
+        .to_string())
 }
--- 2023/day-1/src/part_2.rs
+++ 2023/day-1/src/part_2.rs
@@ -11,24 +11,25 @@ const NUMS: [(&'static str, &'static str); 9] = [
 ];
 
 pub fn process(_input: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let result = _input.lines().fold(0, |mut ans, mut _line| {
-        let mut line_value = String::from(_line);
-        for (num_str, replacement) in NUMS {
-            line_value = line_value.clone().replace(
-                num_str,
-                (num_str.to_string() + replacement + num_str).as_str(),
-            );
-        }
+    Ok(_input
+        .lines()
+        .fold(0, |mut ans, mut _line| {
+            let mut line_value = String::from(_line);
+            for (num_str, replacement) in NUMS {
+                line_value = line_value.clone().replace(
+                    num_str,
+                    (num_str.to_string() + replacement + num_str).as_str(),
+                );
+            }
 
-        let nums = line_value
-            .chars()
-            .filter_map(|x| x.to_digit(10))
-            .collect::<Vec<u32>>();
+            let nums = line_value
+                .chars()
+                .filter_map(|x| x.to_digit(10))
+                .collect::<Vec<u32>>();
 
-        ans += (10 * nums.first().unwrap_or(&0)) + nums.last().unwrap_or(&0);
+            ans += (10 * nums.first().unwrap_or(&0)) + nums.last().unwrap_or(&0);
 
-        ans
-    });
-
-    Ok(result.to_string())
+            ans
+        })
+        .to_string())
 }
