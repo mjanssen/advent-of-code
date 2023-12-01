575b16748ef75b9a76ced51adc65a307436623a8
mjanssen <dev.marnix@gmail.com>
2023-12-01 15:08:44 +0100

    chore: cleaned up first/last option

--- 2023/day-1/src/part_1.rs
+++ 2023/day-1/src/part_1.rs
@@ -5,19 +5,7 @@ pub fn process(_input: &str) -> Result<String, Box<dyn std::error::Error>> {
             .filter_map(|x| x.to_digit(10))
             .collect::<Vec<u32>>();
 
-        let mut str = String::new();
-
-        if let Some(first) = nums.first() {
-            str.push_str(&first.to_string());
-        }
-
-        if let Some(last) = nums.last() {
-            str.push_str(&last.to_string());
-        }
-
-        if let Ok(n) = str.parse::<i32>() {
-            ans += n;
-        }
+        ans += (10 * nums.first().unwrap_or(&0)) + nums.last().unwrap_or(&0);
 
         ans
     });
