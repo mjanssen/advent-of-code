ea20deea8d705ecc3304a610491284dad3470aca
Marnix Janssen <dev.marnix@gmail.com>
2024-12-05 08:43:20 +0100

    day 4 base

---
+++ 2024/day-04/src/bin/part_1.rs
@@ -0,0 +1,15 @@
+use std::time::Instant;
+
+use day_04::part_1::process;
+
+pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let start: Instant = Instant::now();
+
+    let file = include_str!("../../input_1.txt");
+    let result = process(file);
+
+    println!("{:?}", result);
+    println!("Execution time: {:.3?}", start.elapsed());
+
+    Ok(())
+}
---
+++ 2024/day-04/src/part_2.rs
@@ -0,0 +1,3 @@
+pub fn process(_input: &str) -> Result<String, Box<dyn std::error::Error>> {
+    todo!("day_04 - part 2");
+}
