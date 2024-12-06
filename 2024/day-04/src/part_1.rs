9bb8e56079f47d2f101ac0336a112bb54e7d4781
Marnix Janssen <dev.marnix@gmail.com>
2024-12-06 15:35:45 +0100

    day 4

--- 2024/day-04/src/part_1.rs
+++ 2024/day-04/src/part_1.rs
@@ -1,3 +1,95 @@
+use std::{collections::HashMap, ops::RangeInclusive};
+
+const SEARCH: [&str; 1] = ["XMAS"];
+
+fn dir_iter() -> RangeInclusive<i16> {
+    (0..=3).into_iter()
+}
+
 pub fn process(_input: &str) -> Result<String, Box<dyn std::error::Error>> {
-    todo!("day_04 - part 1");
+    let map = _input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .map(move |(x, char)| ((x as i16, y as i16), char))
+        })
+        .collect::<HashMap<(i16, i16), _>>();
+
+    let res = map
+        .iter()
+        .map(|((x, y), _)| {
+            let mut count = 0;
+
+            // Left
+            let left = dir_iter()
+                .filter_map(|i| map.get(&(x + i, *y)))
+                .collect::<String>();
+
+            if SEARCH.contains(&left.as_str()) {
+                count += 1;
+            }
+
+            let right = dir_iter()
+                .filter_map(|i| map.get(&(x - i, *y)))
+                .collect::<String>();
+
+            if SEARCH.contains(&right.as_str()) {
+                count += 1;
+            }
+
+            let up = dir_iter()
+                .filter_map(|i| map.get(&(*x, y - i)))
+                .collect::<String>();
+
+            if SEARCH.contains(&up.as_str()) {
+                count += 1;
+            }
+
+            let down = dir_iter()
+                .filter_map(|i| map.get(&(*x, y + i)))
+                .collect::<String>();
+
+            if SEARCH.contains(&down.as_str()) {
+                count += 1;
+            }
+
+            let left_up = dir_iter()
+                .filter_map(|i| map.get(&(x - i, y - i)))
+                .collect::<String>();
+
+            if SEARCH.contains(&left_up.as_str()) {
+                count += 1;
+            }
+
+            let right_up = dir_iter()
+                .filter_map(|i| map.get(&(x + i, y - i)))
+                .collect::<String>();
+
+            if SEARCH.contains(&right_up.as_str()) {
+                count += 1;
+            }
+
+            let left_down = dir_iter()
+                .filter_map(|i| map.get(&(x - i, y + i)))
+                .collect::<String>();
+
+            if SEARCH.contains(&left_down.as_str()) {
+                count += 1;
+            }
+
+            let right_down = dir_iter()
+                .filter_map(|i| map.get(&(x + i, y + i)))
+                .collect::<String>();
+
+            if SEARCH.contains(&right_down.as_str()) {
+                count += 1;
+            }
+
+            count
+        })
+        .sum::<u16>();
+
+    Ok(res.to_string())
 }
--- 2024/day-04/src/part_2.rs
+++ 2024/day-04/src/part_2.rs
@@ -1,3 +1,60 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
+
+const SEARCH: [&str; 2] = ["MS", "SM"];
+
+fn dir_iter() -> Vec<i16> {
+    (-1..2).collect_vec()
+}
+
+trait CollectMatches {
+    fn collect_matches(self) -> String;
+}
+
+impl<'a, I> CollectMatches for I
+where
+    I: Iterator<Item = &'a char>,
+{
+    fn collect_matches(self) -> String {
+        self.filter(|&&c| matches!(c, 'M' | 'S')).copied().collect()
+    }
+}
+
 pub fn process(_input: &str) -> Result<String, Box<dyn std::error::Error>> {
-    todo!("day_04 - part 2");
+    let map = _input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .map(move |(x, char)| ((x as i16, y as i16), char))
+        })
+        .collect::<HashMap<(i16, i16), _>>();
+
+    let res = map
+        .iter()
+        .filter(|(_, char)| char == &&"A".chars().next().unwrap())
+        .map(|((x, y), _)| {
+            let mut count = 0;
+
+            let left_up = dir_iter()
+                .into_iter()
+                .filter_map(|i| map.get(&(x - i, y - i)))
+                .collect_matches();
+
+            let right_up = dir_iter()
+                .into_iter()
+                .filter_map(|i| map.get(&(x + i, y - i)))
+                .collect_matches();
+
+            if SEARCH.contains(&right_up.as_str()) && SEARCH.contains(&left_up.as_str()) {
+                count += 1;
+            }
+
+            count
+        })
+        .sum::<u16>();
+
+    Ok(res.to_string())
 }
